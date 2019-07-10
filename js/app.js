e761d330277526b428eeff6041f2ddcb9a2e045c
khurshed52 <khurshedkhan52@gmail.com>
2019-07-10 22:39:52 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.dis'),
+    intensity: 0.3,
+    image1: '../images/1.jpg',
+    image2: '../images/2.jpg',
+    angle2: Math.PI / 8,
+    displacementImage: '../images/stripe.png'
+});
